0a0e7ef951f01b62b25e2f3d406e39782ec23999
Nazar <Nazar@DESKTOP-6G6I8HO>
2022-11-05 15:19:32 +0500

    Добавьте файлы проекта.

---
+++ GetTitleWithSelenium/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
+namespace GetTitleWithSelenium
+{
+    internal class Program
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        static void Main(string[] args)
+        {
+            IWebDriver WebDriver = new ChromeDriver();
+            WebDriver.Navigate().GoToUrl("https://google.com");
+            WebDriver.FindElement(By.XPath("//input[@name=\"q\"]")).SendKeys("Hello World");
+            WebDriver.FindElement(By.XPath("//input[@name=\"btnK\"]")).SendKeys(Keys.Enter);
+
+            string GetTitle = WebDriver.Title;
+            Console.ForegroundColor = ConsoleColor.Red;
+            log.Info($"{GetTitle}");
+            Console.ResetColor();
+
+
+
+        }
+    }
+}
